21c4ffef7efd37ec26077830ffc22e2c87a17e11
Drew Szurko <andrewszurko@gmail.com>
2017-12-20 03:06:34 -0600

    Confirmed GC ML Engine does support --runtime-version 1.4.

--- cloud_train.sh
+++ cloud_train.sh
@@ -7,13 +7,10 @@ export JOB_NAME=$FILENAME
 export JOB_DIR=gs://$BUCKET_NAME/$JOB_NAME
 export REGION=us-central1
 
-# --runtime-version specifies the TensorFlow version to use on the GC ML Engine.
-# As of 12/19/2017, --runtime-version 1.2 is the most current version that supports GPU learning.
-
 gcloud ml-engine jobs submit training $JOB_NAME \
     --job-dir $JOB_DIR \
     --region $REGION \
-    --runtime-version 1.2 \
+    --runtime-version 1.4 \
     --module-name trainer.main \
     --config=trainer/config.yaml \
     --package-path ./trainer \
